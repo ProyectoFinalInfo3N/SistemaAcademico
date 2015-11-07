1fa1c41bd7814a0d66f480751b2ced948391f949
Luis Orjuela <lhorjuelag@hotmail.com>
2015-11-07 03:57:07 -0500

    commit071120150357

--- src/main/java/com/uelbosque/cjlfactory/test/MainHibernateTest.java
+++ src/main/java/com/uelbosque/cjlfactory/test/MainHibernateTest.java
@@ -1,21 +1,19 @@
 package com.uelbosque.cjlfactory.test;
 
-
 import com.uelbosque.cjlfactory.DAO.ImplEstudianteDAO;
 import com.uelbosque.cjlfactory.model.Estudiante;
-
 
 public class MainHibernateTest {
 
 	public static void main(String[] args) throws Exception {
 
 		ImplEstudianteDAO generic =  new ImplEstudianteDAO();
-		Estudiante estudianteRecuperado =  generic.getById(1018433394);
+		Estudiante estudianteRecuperado =  generic.getById(1);
 		if(estudianteRecuperado != null)
-			System.out.println(estudianteRecuperado.getNombres());
+			System.out.println(estudianteRecuperado.getNombre());
 		
 		estudianteRecuperado = new Estudiante();
-		estudianteRecuperado.setNombres("Prueba Nombre con JPA");
+		estudianteRecuperado.setNombre("Prueba Nombre con JPA 3:53 de la mañana");
 		estudianteRecuperado.setApellidos("Prueba Apellidos");
 		generic.create(estudianteRecuperado);
 		
---
+++ src/main/resource/com/uelbosque/cjlfactory/model/Materia_.java
@@ -0,0 +1,13 @@
+package com.uelbosque.cjlfactory.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-11-07T03:24:30.354-0500")
+@StaticMetamodel(Materia.class)
+public class Materia_ {
+	public static volatile SingularAttribute<Materia, Integer> idMateria;
+	public static volatile SingularAttribute<Materia, String> nombreMateria;
+	public static volatile SingularAttribute<Materia, Integer> creditosMaterias;
+}
